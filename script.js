ba841efbd8e0103629c526a75718086a007bb1c7
Pavankumar098 <95673383+Pavankumar098@users.noreply.github.com>
2024-02-11 22:12:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,43 +3,28 @@ window.onload = function() {
     var container = document.getElementById("container");
 
     container.addEventListener("click", function(e) {
-        buttonClick(e.target.id);
+        buttonClick(e.target);
     });
 
-    var calc = document.getElementById("Button=");
-    calc.addEventListener("click",calculate);
-
-    var C = document.getElementById("ButtonC");
-    C.addEventListener("click", erase);
-
-    function buttonClick(buttonId) {
-        if((buttonId != "ButtonC") && (buttonId != "Button=")) {
-            var button = document.getElementById(buttonId);
-             var s = buttonId;
-             s = s.replace("Button", "");
-             entries(s);
+    function buttonClick(button) {
+        if(button.tagName === "INPUT") {
+            var buttonValue = button.value;
+            if(buttonValue !== "C" && buttonValue !== "=") {
+                input.value += buttonValue;
+            } else if(buttonValue === "C") {
+                erase();
+            } else if(buttonValue === "=") {
+                calculate();
+            }
         }
-    }
-
-    function entries(s) {
-        input.value += s;
-        /* button1: s = "1"
-        input.value = undefined
-        entries("1")
-        input.value =+ s
-        input.value = input.value + s = undefined + "1" = "1"
-        button2 : s = "2"
-        input.value ="1"
-        entries("2")
-        input.value = input.value + s = "1" + "2" ="12"
-        */
     }
 
     function calculate() {
-        if(input.value == ".") {
-            alert("Please Enter a Mathematical Expression");
+        try {
+            input.value = eval(input.value);
+        } catch(error) {
+            alert("Invalid expression");
         }
-        input.value = eval(input.value);
     }
 
     function erase() {
